667dddcde7096e7e994429d4cbed35e03fcb1fe0
Aivar Mägi <aivar.magi@cgi.com>
2017-11-01 12:22:04 +0200

    refactor build.gradle -> build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+
+    extra["kotlinVersion"] = "1.1.51"
+    extra["springBootVersion"] = "2.0.0.M5"
+
+    val springBootVersion: String by extra
+
+    repositories {
+        mavenCentral()
+        maven("https://repo.spring.io/milestone")
+    }
+
+    dependencies {
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:${springBootVersion}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${kotlinVersion}")
+        classpath("org.jetbrains.kotlin:kotlin-allopen:${kotlinVersion}")
+    }
+}
+
+plugins {
+    val kotlinVersion = "1.1.51"
+
+    id("org.jetbrains.kotlin.jvm") version kotlinVersion
+    id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
+    id("io.spring.dependency-management") version "1.0.3.RELEASE"
+}
+
+apply {
+    plugin("org.springframework.boot")
+}
+
+val kotlinVersion: String by extra
+val springBootVersion: String by extra
+
+version = springBootVersion
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.spring.io/milestone")
+}
+
+
+dependencies {
+    compile("org.springframework.boot:spring-boot-starter-data-jpa")
+    compile("org.springframework.boot:spring-boot-starter-thymeleaf")
+    compile("org.springframework.boot:spring-boot-starter-web")
+    compile("org.jetbrains.kotlin:kotlin-stdlib-jre8:${kotlinVersion}")
+    compile("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+
+    testCompile("org.springframework.boot:spring-boot-starter-test")
+
+    runtime("org.springframework.boot:spring-boot-devtools")
+    runtime("com.h2database:h2")
+}

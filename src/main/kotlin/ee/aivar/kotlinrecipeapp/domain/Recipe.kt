c0e3d0db75b654ec154257b351c3141b07901a2c
Aivar Mägi <aivar.magi@cgi.com>
2017-11-01 12:32:19 +0200

    add recipe

---
+++ src/main/kotlin/ee/aivar/kotlinrecipeapp/domain/Recipe.kt
@@ -0,0 +1,33 @@
+package ee.aivar.kotlinrecipeapp.domain
+
+import javax.persistence.*
+
+@Entity
+open class Recipe : BaseEntity() {
+
+    var description: String? = null
+    var prepTime: Int? = null
+    var cookTime: Int? = null
+    var servings: Int? = null
+    var source: String? = null
+    var url: String? = null
+    var directions: String? = null
+
+    @OneToMany(cascade = arrayOf(CascadeType.ALL), mappedBy = "recipe")
+    var ingredients: Set<Ingredient> = HashSet()
+
+    @Lob
+    var image: ByteArray? = null
+
+    @Enumerated(value = EnumType.STRING)
+    var difficulty: Difficulty? = null
+
+    @OneToOne(cascade = arrayOf(CascadeType.ALL))
+    var notes: Notes? = null
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+        joinColumns = arrayOf(JoinColumn(name = "recipe_id")),
+        inverseJoinColumns = arrayOf(JoinColumn(name = "category_id")))
+    var categories: Set<Category> = HashSet()
+}
